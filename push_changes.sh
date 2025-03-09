c010d13ba42997c906533aa7bbc697a204a84e8a
Gurbakhshish Singh <gurbakhshish.s@gmail.com>
2025-03-09 13:32:54 +0000

    auto sync at 2025-03-09 13:32:54

--- push_changes.sh
+++ push_changes.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd $SCRIPT_DIR
 
-git pull --rebase
 git commit -am "auto sync at $(date '+%Y-%m-%d %H:%M:%S')"
+git pull --rebase
 git push
 cd -
